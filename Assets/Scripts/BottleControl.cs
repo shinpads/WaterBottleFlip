28e06701d02ab887b643bdc6e40516efcb201502
marshmelllo <robfarlow@gmail.com>
2016-09-20 16:31:49 -0400

    walls, jumping

--- Assets/Scripts/BottleControl.cs
+++ Assets/Scripts/BottleControl.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class BottleControl : MonoBehaviour {
-
+	Rigidbody rb;
+	bool touched = false;
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.touchCount > 1 && touched == false)
+			touched = true;
+		else
+			touched = false;
+		if (touched || Input.GetMouseButtonDown(0)) {
+			rb.AddForce (new Vector3 (0, 300, 0));
+		}
 	}
 }
